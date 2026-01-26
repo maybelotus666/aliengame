25bfebb47c1b36edcf7e3050c0c48f6c199a6bba
maybelotus666 <sthefany.cmoreira@senacsp.edu.br>
2026-01-25 22:55:54 -0300

    atualizado de novo! add animação del paginas de carregamento

---
+++ site/paginadejogo/jogo.js
@@ -0,0 +1,21 @@
+const emojis=["☠️","💎","✨"];
+const itens = document.querySelectorAll('.item');
+function girar () {
+   let contador = 0;
+    const intervalo = setInterval(() => { itens.forEach(item => { 
+        const emojiAleatorio = 
+        emojis[Math.floor(Math.random() * emojis.length)];
+        item.textContent = emojiAleatorio; 
+    });
+    contador++;
+
+    if (contador > 20 ) {
+        clearInterval(intervalo);
+    }
+    }, 100);
+}
+
+document.querySelector('.botao-girar').addEventListener('click', girar);
+
+item.classList.add('girando');
+item.classList.remove('girando');
